b71850fc334349568f8ee2d464f6c84e76090d0b
João Carlos Pereira <jcp2105@gmail.com>
2018-07-04 21:34:01 -0300

    Iniciado visual

--- trabalhofinalnode/app.js
+++ trabalhofinalnode/app.js
@@ -1,41 +1,43 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require('express')
+	, load = require('consign')
+	, cookieParser = require('cookie-parser')
+	, session = require('express-session')
+	, bodyParser = require('body-parser')
+	, methodOverride = require('method-override')
+	, error = require('./middleware/error')
+	, mongoose = require('mongoose')
+	, app = express();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+app.use(cookieParser('ntalk'));
+app.use(session({
+	secret: "abc123",
+	name: 'ntalk',
+	resave: true,
+	saveUninitialized: true
+}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(methodOverride('_method'));
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/views'));
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+mongoose.connect('mongodb://localhost/ntalk');
+global.db = mongoose.connection;
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+load().include('models')
+	.then('controllers')
+	.then('routes')
+	.into(app);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// Executa após as rotas se não encontrado
+app.use(error.notFound);
+// Executa ao ocorrer um erro
+app.use(error.serverError);
+
+
+
+app.listen(3000, function () {
+	console.log("Ntalk no ar.");
 });
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
