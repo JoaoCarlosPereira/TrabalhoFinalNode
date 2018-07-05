f9f2377fd01c121f3fdd5a7542c63db8016f59d1
Fhelipe Eduardo Carvalho <fhepez@gmail.com>
2018-07-05 20:04:01 -0300

    removido url_imagem e incluido o campo de quantidade no objeto do produto

--- trabalhofinalnode/models/produto.js
+++ trabalhofinalnode/models/produto.js
@@ -3,7 +3,7 @@ module.exports = function(app) {
 	var produto = Schema({
 		descricao: {type: String, required: true
 		, codigo: {unique: true}}
-		, url_imagem : {type: String, required: true}
+		, quantidade : {type: String}
 	});
 	return db.model('produtos', produto);
 };
