5d7e5a190312cf6f092a3c1de49173206ceb974d
João Carlos Pereira <jcp2105@gmail.com>
2018-07-06 20:16:00 -0300

    Removidos arquivos desnecessários

--- trabalhofinalnode/views/assets/js/funcoes.js
+++
@@ -1,9 +0,0 @@
-function habilitarProduto() {
-    document.getElementById("produto").style.display = 'block';
-    document.getElementById("lista").style.display = 'none';
-}
-
-function habilitaLista() {
-    document.getElementById("lista").style.display = 'block';
-    document.getElementById("produto").style.display = 'none';
-}
