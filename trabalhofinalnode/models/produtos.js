7e255b52aa7d40c37b389f03d576788ba031e6b0
Jaíne Seibert <jaineseibert1996@gmail.com>
2018-07-05 21:57:54 -0300

    removido arquivo desncessário que causava erro

--- trabalhofinalnode/models/produtos.js
+++ trabalhofinalnode/models/produtos.js
@@ -1,11 +1,11 @@
 module.exports = function (app) {
 	var Schema = require('mongoose').Schema;
-	var produto = Schema({
+	var produtos = Schema({
 		descricao: {
 			type: String, required: true
 			, index: { unique: true }
 		}
 		, quantidade: { type: Number, required: true }
 	});
-	return db.model('produtos', produto);
+	return db.model('produtos', produtos);
 };
